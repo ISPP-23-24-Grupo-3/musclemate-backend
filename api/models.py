0c5a8b55b4641e35420b7968fa49a30d697497d9
AleSosa13 <alesoscor@alum.us.es>
2024-02-23 13:35:25 +0100

    Introducidas funciones CRUD para Gym

--- api/models.py
+++ api/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Gym(models.Model):
+    name = models.CharField(max_length = 50)
+    address = models.CharField(max_length = 200)
+    phone_number = models.IntegerField()
+    descripcion = models.CharField(max_length = 500)
+    zip_code = models.IntegerField()
+    email = models.EmailField()
+    #owner = models.ForeignKey(Owner, on_delete=models.CASCADE)
--- musclematebackend/urls.py
+++ musclematebackend/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from api import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('gyms/', views.gym_list),
+    path('gyms/<int:id>/', views.gym_detail),
+    path('gyms/delete/<int:id>/', views.gym_delete),
 ]
