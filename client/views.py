c5d9f96db1a65f2c3c028cd8c2ab7c10ed5ddb61
David Dana <davdancab@alum.us.es>
2024-02-24 01:25:23 +0100

    feat: crud de client #14

--- client/views.py
+++ client/views.py
@@ -1,3 +1,38 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework import status
+from .models import Client
 
-# Create your views here.
+
+class ClientListView(APIView):
+    def get(self, request):
+        clients = Client.objects.all()
+        return Response(clients.data)
+    
+class ClientDetailView(APIView):
+    def get(self, request,pk):
+        client = Client.objects.get(pk=pk)
+        if client:
+            return Response({client.data})
+        else:
+            return Response({"error": "Client not found"}, status=status.HTTP_404_NOT_FOUND)
+
+class ClientCreateView(APIView):
+    def post(self, request):
+        client= Client(**request.data)
+        client.save()
+        return Response(client.data)
+
+
+class ClientUpdateView(APIView):
+    def post(self, request, pk):
+        client = Client.objects.get(pk=pk)
+        client=client.__dict__.update(request.data)
+        client.save()
+        return Response(client.data)
+
+class ClientDeleteView(APIView):
+    def delete(self, request, pk):
+        client = Client.objects.get(pk=pk)
+        client.delete()
+        return Response('Client deleted')
