9171c44ccc1c9da1c555ae209c4d9fe690f16fe3
agudevbon <agudevbon@alum.us.es>
2024-02-24 15:00:35 +0100

    fix: borrando imports no usados #10

--- ticket/serializers.py
+++ ticket/serializers.py
@@ -1,7 +1,4 @@
 from rest_framework import serializers
-from .models import CustomUser
-
-from rest_framework import serializers
 from .models import Ticket
 
 class TicketSerializer(serializers.ModelSerializer):
