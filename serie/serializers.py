62fddaa0190a98623448bf044a24eae86f20f450
David Dana <davdancab@alum.us.es>
2024-02-26 12:46:59 +0100

    Fix añadidos serializers de serie y workout

---
+++ serie/serializers.py
@@ -0,0 +1,16 @@
+from rest_framework import serializers
+from .models import Serie
+from .models import Workout
+
+class SerieSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Serie
+        fields = '__all__'
+
+    def validate_workout(self, value):
+        """
+        Comprobar si existe el workout proporcionado.
+        """
+        if not Workout.objects.filter(id=value.id).exists():
+            raise serializers.ValidationError("Workout does not exist.")
+        return value
