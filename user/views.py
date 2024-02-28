04ae0176260236b78f940ae216936ed2f326d1bd
David Zarandieta <davzarort@alum.us.es>
2024-02-28 13:41:29 +0100

    Add api_view decorator to get_routes method

--- user/views.py
+++ user/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework.decorators import api_view
 from .models import CustomUser
 from .serializers import CustomUserSerializer
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
@@ -46,7 +47,7 @@ class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
     
 class MyTokenObtainPairView(TokenObtainPairView):
     serializer_class = MyTokenObtainPairSerializer
-    @APIView['GET']
+    @api_view(['GET'])
     def get_routes(request):
         routes = [
             'api/token/',
