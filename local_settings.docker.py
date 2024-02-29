b5a98bdd3c1a41e8d666f931b01a9f7589fb4a69
Elena Tomás Vela <eletomvel@alum.us.es>
2024-02-29 02:34:18 +0100

    fix: add docker settings and allowed its hosts

---
+++ local_settings.docker.py
@@ -0,0 +1,16 @@
+"""
+Setup de la base de datos Postgres
+
+Modificar con los datos que hayas inicializado tu base de datos.
+"""
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "postgres",
+        "USER": "postgres",
+        "PASSWORD": "postgres",
+        "HOST": "db",
+        "PORT": "5432",
+    }
+}
