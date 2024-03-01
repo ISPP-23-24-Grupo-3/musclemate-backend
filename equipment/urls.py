7de4c0dcb95d2c931681f465dd86df2467f80d8e
agudevbon <agudevbon@alum.us.es>
2024-03-01 15:07:29 +0100

    Arreglado error serializer y añadida vista detalle equipment #54

--- equipment/urls.py
+++ equipment/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('equipments/', views.EquipmentListView.as_view(), name='equipment_list'),
+    path('equipments/detail/<int:pk>', views.EquipmentDetailView.as_view(), name='equipment_list'),
     path('equipments/create/', views.EquipmentCreateView.as_view(), name='equipment_create'),
     path('equipments/update/<int:pk>/', views.EquipmentUpdateView.as_view(), name='equipment_update'),
     path('equipments/delete/<int:pk>/', views.EquipmentDeleteView.as_view(), name='equipment_delete'),
