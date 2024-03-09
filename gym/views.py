99844e674ed52aee6570dd9d17bb22d0952bc2c6
David Zarandieta <davzarort@alum.us.es>
2024-03-09 14:23:04 +0100

    Add userCustom and owner fields to GymSerializer and update gym_create view

--- gym/views.py
+++ gym/views.py
@@ -4,6 +4,9 @@ from django.shortcuts import get_object_or_404
 from .models import Gym
 from .serializers import GymSerializer
 from user.serializers import CustomUserSerializer
+from user.models import CustomUser
+from owner.models import Owner
+
 
 @api_view(['GET'])
 def gym_list(request):
@@ -20,17 +23,24 @@ def gym_detail(request, id):
 @api_view(['POST'])
 def gym_create(request):
     if request.method == 'POST':
-        user_serializer = CustomUserSerializer(data=request.data)
+        if request.user.rol != 'admin' and request.user.rol != 'owner':
+            return Response('You are not authorized to create a gym')
+        user_data = request.data.get('userCustom')
+        user_serializer = CustomUserSerializer(data=user_data)
         if user_serializer.is_valid():
-              user = user_serializer.save(role='gym')
-              gym_serializer = GymSerializer(data=request.data)
-              if gym_serializer.is_valid():
-                gym_serializer.save(user=user)
+            user = user_serializer.save(rol='gym')
+            gym_data = request.data
+            gym_data['userCustom'] = user.username
+            owner = Owner.objects.get(userCustom=request.user.username)
+            gym_data['owner'] = owner.id
+            gym_serializer = GymSerializer(data=gym_data)
+            if gym_serializer.is_valid():
+                gym_serializer.save()
                 return Response(gym_serializer.data, status=201)
-              else:
+            else:
                 return Response(gym_serializer.errors, status=400)
         else:
-              return Response(user_serializer.errors, status=400)
+            return Response(user_serializer.errors, status=400)
 
 @api_view(['PUT'])
 def gym_update(request, id):
