88a44fb3d1353ded948532d9d1eef0052454a986
David Dana <davdancab@alum.us.es>
2024-03-11 14:17:26 +0100

    feat: requisitos routine

--- routine/urls.py
+++ routine/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 urlpatterns = [
     path('routines/', views.RoutineListView.as_view(), name='routine_list'),
+    path('routines/client/<int:clientId>/', views.RoutineListByClientView.as_view(), name='routine_list_By_Client'),
+    path('routines/detail/<int:pk>/', views.RoutineDetailView.as_view(), name='routine_detail'),
     path('routines/create/', views.RoutineCreateView.as_view(), name='routine_create'),
     path('routines/update/<int:pk>/', views.RoutineUpdateView.as_view(), name='routine_update'),
     path('routines/delete/<int:pk>/', views.RoutineDeleteView.as_view(), name='routine_delete'),
