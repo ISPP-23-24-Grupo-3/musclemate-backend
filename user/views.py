161f658fa5ebf1e66c338115c86f89eefcb85f1f
AleSosa13 <alesoscor@alum.us.es>
2024-03-12 18:14:19 +0100

    fix: arreglado endpoint de edición de clientes

--- user/views.py
+++ user/views.py
@@ -8,39 +8,37 @@ from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.decorators import api_view, permission_classes
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 
+@permission_classes([IsAuthenticated, IsAdminUser])
 class UserListView(APIView):
     def get(self, request):
-        if request.user.rol != 'admin':
-            return Response('You are not authorized to view this users')
         users = CustomUser.objects.all()
         serializer = CustomUserSerializer(users, many=True)
         return Response(serializer.data)
-    
+
+@permission_classes([IsAuthenticated])    
 class UserDetailView(APIView):
     def get(self, request, username):
-        if (request.user.username == username) or (request.user.rol == 'admin'):
+        if (request.user.username == username) or request.user.is_staff:
             user = CustomUser.objects.get(username=username)
             serializer = CustomUserSerializer(user)
             return Response(serializer.data)
         else:
             return Response('You are not authorized to view this user')
 
+@permission_classes([IsAuthenticated, IsAdminUser])
 class UserCreateView(APIView):
     def post(self, request):
-        if request.user.rol != 'admin':
-            return Response('You are not authorized to create a user')
         serializer = CustomUserSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
         return Response(serializer.errors)
 
+@permission_classes([IsAuthenticated, IsAdminUser])
 class UserUpdateView(APIView):
     def put(self, request, username):
-        if request.user.rol != 'admin':
-            return Response('You are not authorized to view this user')
         user = CustomUser.objects.get(username=username)
         serializer = CustomUserSerializer(user, data=request.data)
         if serializer.is_valid():
@@ -48,16 +46,13 @@ class UserUpdateView(APIView):
             return Response(serializer.data)
         return Response(serializer.errors)
        
-
+@permission_classes([IsAuthenticated, IsAdminUser])
 class UserDeleteView(APIView):
     def delete(self, request, pk):
-        if request.user.rol != 'admin':
-            return Response('You are not authorized to view this user')
         user = CustomUser.objects.get(pk=pk)
         user.delete()
         return Response('User deleted')
     
-
 class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
     @classmethod
     def get_token(cls, user):
