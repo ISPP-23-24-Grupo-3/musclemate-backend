4376304c5c612b7d1548e3b98c91ea9e257dc2c3
agudevbon <agudevbon@alum.us.es>
2024-03-13 21:04:21 +0100

    feat: ticket list By
    añadido el listado de tickets por cliente o por maquina y sus restricciones de seguridad

--- ticket/views.py
+++ ticket/views.py
@@ -1,18 +1,90 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from gym.models import Gym
+from rest_framework.decorators import permission_classes
+from rest_framework.permissions import IsAuthenticated
+from owner.models import Owner
 from .models import Ticket
 from .serializers import TicketSerializer, TicketViewSerializer, TicketUpdateSerializer
 from rest_framework.permissions import IsAuthenticated
 from client.models import Client
 from user.models import CustomUser
+from equipment.models import Equipment
 
+def getClientFromUser(username):
+    user = CustomUser.objects.get(username=username)
+    client = Client.objects.get(user=user)
+    return client
+
+def getOwnerFromUser(username):
+    user = CustomUser.objects.get(username=username)
+    owner = Owner.objects.get(userCustom=user)
+    return owner
+
+def getGymFromUser(username):
+    user = CustomUser.objects.get(username=username)
+    gym = Gym.objects.get(userCustom=user)
+    print(gym)
+    return gym
+
+@permission_classes([IsAuthenticated])
 class TicketListView(APIView):
     def get(self, request):
         tickets = Ticket.objects.all()
         serializer = TicketViewSerializer(tickets, many=True)
         return Response(serializer.data)
 
+@permission_classes([IsAuthenticated])
+class TicketListByClientView(APIView):
+
+    def clientAuthority(self, user, client):
+        has_authority = False
+        if user.rol == "client":
+                if getClientFromUser(user).id == client.id:
+                    has_authority=True
+        elif user.rol == "owner":
+                if getOwnerFromUser(user).id == client.gym.owner.id:
+                    has_authority=True
+        elif user.rol == "gym":
+                if getGymFromUser(user).id == client.gym.id:
+                    has_authority=True
+        return has_authority
+    
+    def get(self, request, clientId):
+        client = Client.objects.get(id=clientId)
+        user = request.user
+        if self.clientAuthority(user, client):
+            tickets = Ticket.objects.filter(client=client)
+            serializer = TicketViewSerializer(tickets, many=True)
+            return Response(serializer.data)
+        else:
+            return Response(status=403)
+
+@permission_classes([IsAuthenticated])        
+class TicketListByEquipmentView(APIView):
+
+    def equipmentAuthority(self, user, equipment):
+        has_authority = False
+        if user.rol == "owner":
+                if getOwnerFromUser(user).id == equipment.gym.owner.id:
+                    has_authority=True
+        elif user.rol == "gym":
+                if getGymFromUser(user).id == equipment.gym.id:
+                    has_authority=True
+        return has_authority
+    
+    def get(self, request, equipmentId):
+        equipment = Equipment.objects.get(id=equipmentId)
+        user = request.user
+        if self.equipmentAuthority(user, equipment):
+            tickets = Ticket.objects.filter(equipment=equipment, gym=equipment.gym)
+            serializer = TicketViewSerializer(tickets, many=True)
+            return Response(serializer.data)
+        else:
+            return Response(status=403)
+
+@permission_classes([IsAuthenticated])
 class TicketCreateView(APIView):
     permission_classes = [IsAuthenticated]
     def post(self, request):
@@ -23,13 +95,15 @@ class TicketCreateView(APIView):
             serializer.save(client=client, gym=client.gym)
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
+
+@permission_classes([IsAuthenticated])    
 class TicketDetailView(APIView):
     def get(self, request,pk):
         serie = Ticket.objects.get(pk=pk)
         serializer=TicketViewSerializer(serie)
         return Response(serializer.data)
 
+@permission_classes([IsAuthenticated])
 class TicketUpdateView(APIView):
     def get_object(self, pk):
         try:
