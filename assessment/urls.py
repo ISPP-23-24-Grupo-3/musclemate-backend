731b63599faaa6aba03bb45d10fc90b0a8cac710
agudevbon <agudevbon@alum.us.es>
2024-03-14 14:35:28 +0100

    feat: list assessment by client

--- assessment/urls.py
+++ assessment/urls.py
@@ -5,6 +5,8 @@ urlpatterns = [
     path('assessments/', views.AssessmentListView.as_view(), name='assessment_list'),
     path('assessments/client/<int:clientId>/', views.AssessmentListByClientView.as_view(),
         name='assessment_list_by_client'),
+    path('assessments/equipment/<int:equipmentId>/', views.AssessmentListByEquipmentView.as_view(),
+        name='assessment_list_by_equipment'),
     path('assessments/detail/<int:pk>/', views.AssessmentDetailView.as_view(), name='assessment_detail'),
     path('assessments/create/', views.AssessmentCreateView.as_view(), name='assessment_create'),
     path('assessments/update/<int:pk>/', views.AssessmentUpdateView.as_view(), name='assessment_update'),
