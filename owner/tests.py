d8f3a83623b691f1e9bbb8dec4c99cc3a9185f76
David Dana <davdancab@alum.us.es>
2024-03-16 17:45:20 +0100

    fix: arreglos de multiples test

--- owner/tests.py
+++ owner/tests.py
@@ -1,6 +1,5 @@
 from django.test import TestCase
 from rest_framework.test import APIRequestFactory
-
 from user.models import CustomUser
 from .models import Owner
 from .views import OwnerListView, OwnerDetailView, OwnerCreateView, OwnerUpdateView, OwnerDeleteView
@@ -8,8 +7,10 @@ from .views import OwnerListView, OwnerDetailView, OwnerCreateView, OwnerUpdateV
 class OwnerAPITestCase(TestCase):
     def setUp(self):
         self.factory = APIRequestFactory()
-        self.user = CustomUser.objects.create(username='test_user', email='test@example.com', rol='owner')
-        self.user1 = CustomUser.objects.create(username='test_user_1', email='test1@example.com', rol='owner')
+        self.user = CustomUser.objects.create(username='testuser', email='test@example.com', 
+            rol='owner', password='testpassword')
+        self.user1 = CustomUser.objects.create(username='test_user_1', email='test1@example.com',
+            rol='owner',password='testpassword')
         self.owner = Owner.objects.create(
             name='John',
             lastName='Doe',
@@ -20,30 +21,31 @@ class OwnerAPITestCase(TestCase):
         )
 
     def test_owner_list_view(self):
-        request = self.factory.get('/owners/')
-        response = OwnerListView.as_view()(request)
+        request = self.factory.get('d')
+        view = OwnerListView.as_view()
+        response=view(request)
         self.assertEqual(response.status_code, 200)
 
     def test_owner_detail_view(self):
-        request = self.factory.get('/owners/')
+        request = self.factory.get('/owners/detail/')
         response = OwnerDetailView.as_view()(request, pk=self.owner.pk)
         self.assertEqual(response.status_code, 200)
 
     def test_owner_create_view(self):
-        data = {'name':'John','lastName':'Doe','email':'john.doe@example.com','phoneNumber':123456789,
-                'address':'123 Owner St','userCustom':self.user1.pk}
-        request = self.factory.post('/owners/', data)
+        data = {'name':'John','lastName':'Doe','email':'john.doe1@example.com','phoneNumber':123456789,
+                'address':'123 Owner St','username': 'jaime99','password': 'yourpassword'}
+        request = self.factory.post('/owners/create/', data)
         response = OwnerCreateView.as_view()(request)
         self.assertEqual(response.status_code, 201)
 
     def test_owner_update_view(self):
-        data = {'name':'Update','lastName':'Doe','email':'john.doe@example.com','phoneNumber':123456789,
+        data = {'name':'Update','lastName':'Doe','email':'john.doe2@example.com','phoneNumber':123456789,
                 'address':'123 Owner St','userCustom':self.user.pk}
-        request = self.factory.post('/owners/', data)
+        request = self.factory.put('/owners/update/', data)
         response = OwnerUpdateView.as_view()(request, pk=self.owner.pk)
         self.assertEqual(response.status_code, 200)
 
     def test_owner_delete_view(self):
-        request = self.factory.delete('/owners/')
+        request = self.factory.delete('/owners/delete/')
         response = OwnerDeleteView.as_view()(request, pk=self.owner.pk)
         self.assertEqual(response.status_code, 200)
