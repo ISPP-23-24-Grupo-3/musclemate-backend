f82b53feaf3c547ecbaeeb6d312ed0aa1919749d
David Zarandieta <davzarort@alum.us.es>
2024-03-28 13:30:33 +0100

    bugfix: Update GymSerializer and Owner model

--- owner/models.py
+++ owner/models.py
@@ -17,7 +17,7 @@ class Owner(models.Model):
     email = models.EmailField()
     phoneNumber=models.IntegerField()
     address=models.CharField(max_length=250)
-    customer_id = models.CharField(max_length=50, unique=True, null = True, default=None)
+    customer_id = models.CharField(max_length=50, unique=True, null = True, default=None, blank=True)
 
     userCustom = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
 
