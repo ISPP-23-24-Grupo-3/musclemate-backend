954afbfb08264d038bfabcdd7969db80d44579ef
AleSosa13 <alesoscor@alum.us.es>
2024-04-11 10:54:08 +0200

    feat: añadido acceso a maquinas desde gimnasio y traducido al español

--- equipment/permissions.py
+++
@@ -1,6 +0,0 @@
-from rest_framework.permissions import BasePermission
-
-class IsGymOwner(BasePermission):
-    def has_permission(self, request, view):
-        # Comprueba si el usuario tiene el rol de dueño
-        return request.user and request.user.is_authenticated and request.user.rol == 'owner'
